02cab91a6dd587af4020298ade2aece38a1ed9ae
Jesse Wolgamott <jesse@comalproductions.com>
2015-07-20 09:52:10 -0500

    Add seeding of heroes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'json'
+Hero.delete_all
+hero_json = JSON.parse File.read("./db/heroes.json")
+
+Hero.create hero_json
+
